335ec5c58a5b25635e39566d7d5cfe5e47223c30
garion92 <47436448+garion92@users.noreply.github.com>
2019-02-14 19:13:52 -0600

    Add files via upload

---
+++ HelloJavaFX/src/application/controller/MainController.java
@@ -0,0 +1,14 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class MainController implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent event) {
+		System.out.println("Hello");
+		
+	}
+	
+}
